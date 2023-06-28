d586b8eb134e9d55c5da438bf9b74bbf194db619
Marlon Baker <marbaker.swe@gmail.com>
2023-06-28 15:35:00 -0700

    updated index.js and store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counterSlice'
-import todoReducer from './features/todoSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './features/counterSlice';
+import todoReducer from './features/todoSlice';
 
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        todos: todoReducer
-    }
-})
+  reducer: {
+    counter: counterReducer,
+    todos: todoReducer
+  },
+  devTools: true 
+});
+
+export default store;
